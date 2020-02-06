fad15bef38ff8c46969852f607f6ec8ecf6ba7d1
Youngkun <youngkunzhu@163.com>
2020-02-06 16:56:16 +0800

    finished

--- demo/settings.py
+++ demo/settings.py
@@ -22,9 +22,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'z8u(n1c00b(gf)6_g7&ki(+)#yz^t$(^c3i_zsgl+33yjnz&n%'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['youngkun.site']
 
 # Application definition
 
@@ -117,3 +117,4 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
+STATIC_ROOT = os.path.join(BASE_DIR, 'collected_static')
