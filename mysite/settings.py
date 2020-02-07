60e91ed28e2572a690971f30322321cb64e0d731
Youngkun <youngkunzhu@163.com>
2020-02-07 23:55:17 +0800

    登录界面提示框添加消除按钮 删除layout作为app的功能

--- mysite/settings.py
+++ mysite/settings.py
@@ -55,6 +55,7 @@ TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [
+            os.path.join(BASE_DIR, 'templates'),
             os.path.join(BASE_DIR, 'layout/templates'),
             os.path.join(BASE_DIR, 'blog/templates'),
             os.path.join(BASE_DIR, 'user/templates'),
